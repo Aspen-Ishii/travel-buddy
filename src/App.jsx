4d1ec31b7e5d5e23ac129a1de52c3f5b88ce2a9f
Aspen Ishii <ishii.aspen@gmail.com>
2024-08-28 09:58:34 -0600

    Add basic CSS to index.css and create form skeleton for adding items to packing list

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 
-
-//imported components
+//custom components
 import Backpack from './components/Backpack';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
-      <h1>Travel Buddy</h1>
+    <div className='container'>
+      <header>
+        <h1>Travel Buddy</h1>
+      </header>
       <Backpack />
       
-     
     </div>
   )
 }
--- src/components/Backpack.jsx
+++ src/components/Backpack.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import styles from './BackpackStyles.module.css';
 
-function Backpack() {
+//prevent auto page refresh when form is submitted 
+const Backpack = () => {
+    const handelFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+
+    }
+
   return (
-    <div>Backpack</div>
-  )
-}
+    <form className="list" onSubmit={handelFormSubmit}>
+        <div className="wrapper"> 
+           <input type='text'  id='item' className='input' 
+           // value={task}
+           //onInput={(e) => setTask(e.target.value)}
+           required 
+           autoFocus
+           maxLength={60}
+           placeholder='Enter item'
+           />
+           <label htmlFor='item' className='label'>Enter Item</label> {/* label moves above input box */}
+        </div>
+        <button className='btn' aria-label='Add Item' type='submit'>
+            Submit
+        </button>
+    </form>
+  );
+};
 
 export default Backpack
